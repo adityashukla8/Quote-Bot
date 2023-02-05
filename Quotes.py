5badf381536fcf4f9b6f390646f2410ad4e5c1b5
Aditya Shukla <53909071+adityashukla8@users.noreply.github.com>
2023-02-05 19:43:03 +0530

    Update Quotes.py

--- Quotes.py
+++ Quotes.py
@@ -1,15 +1,8 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[ ]:
-
-
 from scrapereads import GoodReads
 import random
 import pandas as pd
 
 goodreads = GoodReads()
 
-quotes = goodreads.search_quotes(34583, top_k=5)
-st.write(quotes)
-
+# quotes = goodreads.search_quotes(34583, top_k=5)
+st.write("quotes")
