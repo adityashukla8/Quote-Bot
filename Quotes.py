b311ce49b946b8552ef01859d1f33ef9fadebcaa
Aditya Shukla <53909071+adityashukla8@users.noreply.github.com>
2023-02-07 00:26:44 +0530

    Update Quotes.py

--- Quotes.py
+++ Quotes.py
@@ -11,11 +11,11 @@ dotenv.load_dotenv()
 
 cred = os.environ.get("creds")
 # cred = credentials.Certificate("D:\Aditya\data\Quotes\quotes-904b2-firebase-adminsdk-vrxkf-36da48f48b.json")
-if not firebase_admin._apps:
+if not admin._apps:
     admin.initialize_app(cred)
     ref = db.reference('', url='https://quotes-904b2-default-rtdb.firebaseio.com/')
 else:
-    ref = db.reference('', app=firebase_admin.get_app())
+    ref = db.reference('', app=admin.get_app())
 
 data = ref.get()
 df = pd.DataFrame(data)
