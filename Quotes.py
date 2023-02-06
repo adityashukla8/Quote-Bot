358e3ef295c3826cf4a34dc7f10ad3bd64dee3ff
Aditya Shukla <53909071+adityashukla8@users.noreply.github.com>
2023-02-06 23:46:30 +0530

    Update Quotes.py

--- Quotes.py
+++ Quotes.py
@@ -5,4 +5,8 @@ from firebase_admin import credentials
 from firebase_admin import db
 import streamlit as st
 
+cred = credentials.Certificate("D:\Aditya\data\Quotes\quotes-904b2-firebase-adminsdk-vrxkf-36da48f48b.json")
+admin.initialize_app(cred)
+ref = db.reference('', url='https://quotes-904b2-default-rtdb.firebaseio.com/')
+
 st.write("quotes")
