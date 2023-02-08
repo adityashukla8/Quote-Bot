705050f6698814a4ec12470313e5f50c35f1c9f8
Aditya Shukla <53909071+adityashukla8@users.noreply.github.com>
2023-02-09 00:37:25 +0530

    Update Quotes.py

--- Quotes.py
+++ Quotes.py
@@ -10,14 +10,44 @@ import os
 import requests
 import json
 import airtable
+import random
 
 api_key = 'keyf0HCILmsc89SOu'
 db_key = 'appimdoShD63eN3Zc'
 table = 'Sheet1'
 view = 'Grid view'
 
-url = 'https://api.airtable.com/v0/appimdoShD63eN3Zc/Sheet1?api_key=' + api_key + '&offset=itrMhDF4VD1pxm7W2/rec0PDQhir08dBp1L'
+# url = 'https://api.airtable.com/v0/appimdoShD63eN3Zc/Sheet1?api_key=' + api_key + '&offset=itrMhDF4VD1pxm7W2/rec0PDQhir08dBp1L'
 
-res = requests.get(url)
+# res = requests.get(url)
 
-st.write("quotes")
+authors = ['Richard P. Feynman', 'Leo Tolstoy']
+
+def get_record(author):
+    url = 'https://api.airtable.com/v0/appimdoShD63eN3Zc/Sheet1?api_key=' + api_key + "&filterByFormula=author='" + author + "'"
+    response = requests.get(url)
+    data = response.json()
+    records = data['records']
+    return random.choice(records)
+
+def main():
+    st.title("Author Random Record Generator")
+
+    current_author = st.sidebar.selectbox("Select an author", authors)
+    st.write("Current author:", current_author)
+
+    record = get_record(current_author)
+    st.write("Record:", record)
+
+    if st.button("Generate More"):
+        record = get_record(current_author)
+        st.write("Record:", record)
+
+    if st.sidebar.button("Change Author"):
+        current_author = st.sidebar.selectbox("Select an author", authors)
+        st.write("Current author:", current_author)
+        record = get_record(current_author)
+        st.write("Record:", record)
+
+if __name__ == "__main__":
+    main()
