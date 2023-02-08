d9808f59b4d146d27703b2e02b8a4a4658a179f6
Aditya Shukla <53909071+adityashukla8@users.noreply.github.com>
2023-02-08 14:49:12 +0530

    Update Quotes.py

--- Quotes.py
+++ Quotes.py
@@ -7,17 +7,17 @@ import streamlit as st
 import dotenv
 import os
 
-dotenv.load_dotenv()
+# dotenv.load_dotenv()
 
-cred = os.environ.get("creds")
-# cred = credentials.Certificate("D:\Aditya\data\Quotes\quotes-904b2-firebase-adminsdk-vrxkf-36da48f48b.json")
-if not admin._apps:
-    admin.initialize_app(cred)
-    ref = db.reference('', url='https://quotes-904b2-default-rtdb.firebaseio.com/')
-else:
-    ref = db.reference('', app=admin.get_app())
+# cred = os.environ.get("creds")
+# # cred = credentials.Certificate("D:\Aditya\data\Quotes\quotes-904b2-firebase-adminsdk-vrxkf-36da48f48b.json")
+# if not admin._apps:
+#     admin.initialize_app(cred)
+#     ref = db.reference('', url='https://quotes-904b2-default-rtdb.firebaseio.com/')
+# else:
+#     ref = db.reference('', app=admin.get_app())
 
-data = ref.get()
-df = pd.DataFrame(data)
+# data = ref.get()
+# df = pd.DataFrame(data)
 
 st.write("quotes")
