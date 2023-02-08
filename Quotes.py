33d085e6b465d9517e91272461701ae831f71ec0
Aditya Shukla <53909071+adityashukla8@users.noreply.github.com>
2023-02-08 17:14:17 +0530

    Update Quotes.py

--- Quotes.py
+++ Quotes.py
@@ -3,15 +3,16 @@ import random
 import firebase_admin as admin
 from firebase_admin import credentials
 from firebase_admin import db
+from google.cloud import firestore
 import streamlit as st
 import dotenv
 import os
 
 dotenv.load_dotenv()
 
-cred = credentials.Certificate('quotes-904b2-firebase-adminsdk-vrxkf-36da48f48b.json')
-admin.initialize_app(cred, name = "abcdefadsd")
-ref = db.reference('', url='https://quotes-904b2-default-rtdb.firebaseio.com/')
+# cred = credentials.Certificate('quotes-904b2-firebase-adminsdk-vrxkf-36da48f48b.json')
+# admin.initialize_app(cred, name = "abcdefadsd")
+# ref = db.reference('', url='https://quotes-904b2-default-rtdb.firebaseio.com/')
 
 data = ref.get()
 # df = pd.DataFrame(data)
