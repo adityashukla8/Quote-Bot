7011ab297103ee3dba22e2ce5b9624c82d81624b
Aditya Shukla <53909071+adityashukla8@users.noreply.github.com>
2023-02-09 20:36:58 +0530

    Update Quotes.py

--- Quotes.py
+++ Quotes.py
@@ -47,22 +47,13 @@ def main():
     st.title("Author Random Record Generator")
 
     current_author = change_author()
-#     st.write("Current author:", current_author)
-
-#     record = generate_more(current_author)
-#     st.write("Record:", record)
 
     if st.button("Generate More"):
-#         st.write("Current author:", current_author)
         generate_more(current_author)
-#         st.write("Record:", record)
 
     if st.button("Change Author"):
         current_author = change_author()
         generate_more(current_author)
-#         st.write("Current author:", current_author)
-#         record = generate_more(current_author)
-#         st.write("Record:", record)
 
 if __name__ == "__main__":
     main()
