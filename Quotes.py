8e2624411d7c42196a121d7bf84df117eeb7264f
Aditya Shukla <53909071+adityashukla8@users.noreply.github.com>
2023-02-12 23:29:45 +0530

    Update Quotes.py

--- Quotes.py
+++ Quotes.py
@@ -19,10 +19,9 @@ table = 'Sheet1'
 view = 'Grid view'
 
 authors = ['Richard P. Feynman', 'Leo Tolstoy']
-current_auth = None
+current_author = random.choice(authors)
 current_quotes = None
 
-current_author = random.choice(authors)
 # generate the URL based on the current author
 url = 'https://api.airtable.com/v0/appimdoShD63eN3Zc/Sheet1?api_key=' + api_key + "&filterByFormula=author='" + current_author + "'"
 
@@ -38,5 +37,55 @@ st.write(quote)
 st.write(current_author)
 
 if st.button('Generate More'):
-  st.write(quote)
-  st.write(current_author)
+    global current_author, quotes, quote
+    url = 'https://api.airtable.com/v0/appimdoShD63eN3Zc/Sheet1?api_key=' + api_key + "&filterByFormula=author='" + current_author + "'"
+    response = requests.get(url)
+    data = response.json()
+    quotes = [record['fields']['quote'] for record in data['records']]
+    quote = random.choice(quotes)
+    st.write(quote)
+    st.write(current_author)
+
+
+# import pandas as pd
+# import random
+# import firebase_admin as admin
+# from firebase_admin import credentials
+# from firebase_admin import db
+# from google.cloud import firestore
+# import streamlit as st
+# import dotenv
+# import os
+# import requests
+# import json
+# import airtable
+# import random
+# import requests
+
+# api_key = 'keyf0HCILmsc89SOu'
+# db_key = 'appimdoShD63eN3Zc'
+# table = 'Sheet1'
+# view = 'Grid view'
+
+# authors = ['Richard P. Feynman', 'Leo Tolstoy']
+# current_auth = None
+# current_quotes = None
+
+# current_author = random.choice(authors)
+# # generate the URL based on the current author
+# url = 'https://api.airtable.com/v0/appimdoShD63eN3Zc/Sheet1?api_key=' + api_key + "&filterByFormula=author='" + current_author + "'"
+
+# # make the API request
+# response = requests.get(url)
+# data = response.json()
+
+# quotes = [record['fields']['quote'] for record in data['records']]
+# quote = random.choice(quotes)
+
+# # def get_quote():
+# st.write(quote)
+# st.write(current_author)
+
+# if st.button('Generate More'):
+#   st.write(quote)
+#   st.write(current_author)
